e760a1a89fc46d4af250cea16ecbe19742f5198e
Brice Veyna <bricev99@gmail.com>
2022-08-13 11:05:51 -0700

    Simplify question structure, fix question placement

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,51 +26,45 @@ function displayQuestion() {
     introEl.style.display = "none";
     questionsEl.style.display = "";
 
-    var questionHeaderMain = "";
-    var questionListMain = ["", "", "", ""];
-    var questionHeader = {
-        1: "Commonly used data types do NOT include:",
-        2: "The condition in an if / else statement is enclosed with ______.",
-        3: "Arrays in JavaScript can be used to store ______.",
-        4: "String values must be enclosed within ______ when being assigned to variables.",
-        5: "A very useful tool used during development and debugging for printing content to the debugger is:"
-    };
-    var questionList = {
-        1: ["strings", "booleans", "alerts", "numbers"],
-        2: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-        3: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        4: ["commas", "curly brackets", "quotes", "parenthesis"],
-        5: ["JavaScript", "terminal/bash", "for loops", "console.log"]
-    };
+    const questions = [
+        {
+            questionHeader: "Commonly used data types do NOT include:",
+            questionList: ["strings", "booleans", "alerts", "numbers"],
+            answer: "alerts",
+        },
+        {
+            questionHeader: "The condition in an if / else statement is enclosed with ______.",
+            questionList: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+            answer: "parenthesis",
+        },
+        {
+            questionHeader: "Arrays in JavaScript can be used to store ______.",
+            questionList: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+            answer: "all of the above",
+        },
+        {
+            questionHeader: "String values must be enclosed within ______ when being assigned to variables.",
+            questionList: ["commas", "curly brackets", "quotes", "parenthesis"],
+            answer: "quotes",
+        },
+        {
+            questionHeader: "A very useful tool used during development and debugging for printing content to the debugger is:",
+            questionList: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+            answer: "console.log",
+        },
+    ]
 
-    // Count number of questions in question header object
-    var questionTotal = 0;
-
-    function totalProperties(obj) {
-        for (var property in obj) {
-            if(obj.hasOwnProperty(property))
-            questionTotal++;
+    for (let i = 0; i < questions.length; i++) {
+        questionsEl.children[0].textContent = questions[i].questionHeader;
+        for (let j = 0; j < questions[i].questionList.length; j++) {
+            questionsEl.children[1].children[j].textContent = questions[i].questionList[j];
         }
-    }
-
-    totalProperties(questionHeader);
-    console.log(questionTotal);
-
-    // Iterate over number of questions,
-    for (var i = 0; i < questionTotal; i++) {
-        questionHeaderMain = questionHeader[1];
-        console.log(questionHeaderMain);
-        questionsEl.children[0].textContent = questionHeaderMain;
-        questionListMain = questionList[1];
-        console.log(questionListMain);
-        questionsEl.children[1].textContent = questionListMain;
-        if (answer === true) {
+        if ( choice === questions[i].answer) {
             timeLeft + 10;
         } else {
             timeLeft - 10;
         }
     }
-
 }
 
 // Move to next question, display correct or incorrect message based on click event
